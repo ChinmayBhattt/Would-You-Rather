af69869b6373449445d22b9297073958a572a54e
Chinmay Bhatt <123961463+ChinmayBhattt@users.noreply.github.com>
2025-03-29 00:12:56 +0530

    Add files via upload

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,6 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5701230350599854"
-        crossOrigin="anonymous"
-        strategy="afterInteractive"
-      />
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 } 

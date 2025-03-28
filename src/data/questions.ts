8f66314e31edc6f5683b1e2f738685a739a63a4d
Chinmay Bhatt <123961463+ChinmayBhattt@users.noreply.github.com>
2025-03-28 17:38:25 +0530

    add 100 questions more

--- src/data/questions.ts
+++ src/data/questions.ts
@@ -54,5 +54,455 @@ export const questions: Question[] = [
     id: 10,
     optionA: "Never be able to sleep again",
     optionB: "Never be able to wake up again"
+  },
+  {
+    id: 11,
+    optionA: "Be able to fly but only 2 feet off the ground",
+    optionB: "Be able to breathe underwater but only 2 feet deep"
+  },
+  {
+    id: 12,
+    optionA: "Have a photographic memory",
+    optionB: "Have the ability to forget anything you want"
+  },
+  {
+    id: 13,
+    optionA: "Be able to teleport anywhere instantly",
+    optionB: "Be able to time travel to any point in history"
+  },
+  {
+    id: 14,
+    optionA: "Have unlimited battery life on all your devices",
+    optionB: "Have unlimited storage on all your devices"
+  },
+  {
+    id: 15,
+    optionA: "Be able to speak with animals",
+    optionB: "Be able to speak with plants"
+  },
+  {
+    id: 16,
+    optionA: "Have the power to heal any illness",
+    optionB: "Have the power to cure any addiction"
+  },
+  {
+    id: 17,
+    optionA: "Be able to control fire",
+    optionB: "Be able to control water"
+  },
+  {
+    id: 18,
+    optionA: "Have the ability to clone yourself",
+    optionB: "Have the ability to swap bodies with anyone"
+  },
+  {
+    id: 19,
+    optionA: "Be able to see in the dark",
+    optionB: "Be able to see through walls"
+  },
+  {
+    id: 20,
+    optionA: "Have the power to make anyone fall in love with you",
+    optionB: "Have the power to make anyone tell the truth"
+  },
+  {
+    id: 21,
+    optionA: "Be able to communicate with your past self",
+    optionB: "Be able to communicate with your future self"
+  },
+  {
+    id: 22,
+    optionA: "Have the ability to pause time",
+    optionB: "Have the ability to rewind time"
+  },
+  {
+    id: 23,
+    optionA: "Be able to speak every language in the world",
+    optionB: "Be able to speak with every animal in the world"
+  },
+  {
+    id: 24,
+    optionA: "Have the power to make anyone happy",
+    optionB: "Have the power to make anyone rich"
+  },
+  {
+    id: 25,
+    optionA: "Be able to control the weather",
+    optionB: "Be able to control your dreams"
+  },
+  {
+    id: 26,
+    optionA: "Have the ability to read any book instantly",
+    optionB: "Have the ability to write any book instantly"
+  },
+  {
+    id: 27,
+    optionA: "Be able to see 10 minutes into the future",
+    optionB: "Be able to see 10 minutes into the past"
+  },
+  {
+    id: 28,
+    optionA: "Have the power to make anyone famous",
+    optionB: "Have the power to make anyone invisible"
+  },
+  {
+    id: 29,
+    optionA: "Be able to control gravity",
+    optionB: "Be able to control magnetism"
+  },
+  {
+    id: 30,
+    optionA: "Have the ability to speak with the dead",
+    optionB: "Have the ability to speak with the unborn"
+  },
+  {
+    id: 31,
+    optionA: "Be able to teleport anywhere in the world",
+    optionB: "Be able to fly anywhere in the world"
+  },
+  {
+    id: 32,
+    optionA: "Have the power to make anyone immortal",
+    optionB: "Have the power to make anyone invincible"
+  },
+  {
+    id: 33,
+    optionA: "Be able to control electricity",
+    optionB: "Be able to control sound"
+  },
+  {
+    id: 34,
+    optionA: "Have the ability to speak with your ancestors",
+    optionB: "Have the ability to speak with your descendants"
+  },
+  {
+    id: 35,
+    optionA: "Be able to see through time",
+    optionB: "Be able to see through space"
+  },
+  {
+    id: 36,
+    optionA: "Have the power to make anyone beautiful",
+    optionB: "Have the power to make anyone intelligent"
+  },
+  {
+    id: 37,
+    optionA: "Be able to control plants",
+    optionB: "Be able to control animals"
+  },
+  {
+    id: 38,
+    optionA: "Have the ability to speak with your younger self",
+    optionB: "Have the ability to speak with your older self"
+  },
+  {
+    id: 39,
+    optionA: "Be able to see through lies",
+    optionB: "Be able to see through illusions"
+  },
+  {
+    id: 40,
+    optionA: "Have the power to make anyone happy",
+    optionB: "Have the power to make anyone successful"
+  },
+  {
+    id: 41,
+    optionA: "Be able to control light",
+    optionB: "Be able to control darkness"
+  },
+  {
+    id: 42,
+    optionA: "Have the ability to speak with your past lives",
+    optionB: "Have the ability to speak with your future lives"
+  },
+  {
+    id: 43,
+    optionA: "Be able to see through walls",
+    optionB: "Be able to see through time"
+  },
+  {
+    id: 44,
+    optionA: "Have the power to make anyone rich",
+    optionB: "Have the power to make anyone famous"
+  },
+  {
+    id: 45,
+    optionA: "Be able to control the wind",
+    optionB: "Be able to control the rain"
+  },
+  {
+    id: 46,
+    optionA: "Have the ability to speak with your spirit guide",
+    optionB: "Have the ability to speak with your guardian angel"
+  },
+  {
+    id: 47,
+    optionA: "Be able to see through illusions",
+    optionB: "Be able to see through lies"
+  },
+  {
+    id: 48,
+    optionA: "Have the power to make anyone successful",
+    optionB: "Have the power to make anyone happy"
+  },
+  {
+    id: 49,
+    optionA: "Be able to control the earth",
+    optionB: "Be able to control the sky"
+  },
+  {
+    id: 50,
+    optionA: "Have the ability to speak with your past self",
+    optionB: "Have the ability to speak with your future self"
+  },
+  {
+    id: 51,
+    optionA: "Be able to see through time",
+    optionB: "Be able to see through space"
+  },
+  {
+    id: 52,
+    optionA: "Have the power to make anyone beautiful",
+    optionB: "Have the power to make anyone intelligent"
+  },
+  {
+    id: 53,
+    optionA: "Be able to control plants",
+    optionB: "Be able to control animals"
+  },
+  {
+    id: 54,
+    optionA: "Have the ability to speak with your younger self",
+    optionB: "Have the ability to speak with your older self"
+  },
+  {
+    id: 55,
+    optionA: "Be able to see through lies",
+    optionB: "Be able to see through illusions"
+  },
+  {
+    id: 56,
+    optionA: "Have the power to make anyone happy",
+    optionB: "Have the power to make anyone successful"
+  },
+  {
+    id: 57,
+    optionA: "Be able to control light",
+    optionB: "Be able to control darkness"
+  },
+  {
+    id: 58,
+    optionA: "Have the ability to speak with your past lives",
+    optionB: "Have the ability to speak with your future lives"
+  },
+  {
+    id: 59,
+    optionA: "Be able to see through walls",
+    optionB: "Be able to see through time"
+  },
+  {
+    id: 60,
+    optionA: "Have the power to make anyone rich",
+    optionB: "Have the power to make anyone famous"
+  },
+  {
+    id: 61,
+    optionA: "Be able to control the wind",
+    optionB: "Be able to control the rain"
+  },
+  {
+    id: 62,
+    optionA: "Have the ability to speak with your spirit guide",
+    optionB: "Have the ability to speak with your guardian angel"
+  },
+  {
+    id: 63,
+    optionA: "Be able to see through illusions",
+    optionB: "Be able to see through lies"
+  },
+  {
+    id: 64,
+    optionA: "Have the power to make anyone successful",
+    optionB: "Have the power to make anyone happy"
+  },
+  {
+    id: 65,
+    optionA: "Be able to control the earth",
+    optionB: "Be able to control the sky"
+  },
+  {
+    id: 66,
+    optionA: "Have the ability to speak with your past self",
+    optionB: "Have the ability to speak with your future self"
+  },
+  {
+    id: 67,
+    optionA: "Be able to see through time",
+    optionB: "Be able to see through space"
+  },
+  {
+    id: 68,
+    optionA: "Have the power to make anyone beautiful",
+    optionB: "Have the power to make anyone intelligent"
+  },
+  {
+    id: 69,
+    optionA: "Be able to control plants",
+    optionB: "Be able to control animals"
+  },
+  {
+    id: 70,
+    optionA: "Have the ability to speak with your younger self",
+    optionB: "Have the ability to speak with your older self"
+  },
+  {
+    id: 71,
+    optionA: "Be able to see through lies",
+    optionB: "Be able to see through illusions"
+  },
+  {
+    id: 72,
+    optionA: "Have the power to make anyone happy",
+    optionB: "Have the power to make anyone successful"
+  },
+  {
+    id: 73,
+    optionA: "Be able to control light",
+    optionB: "Be able to control darkness"
+  },
+  {
+    id: 74,
+    optionA: "Have the ability to speak with your past lives",
+    optionB: "Have the ability to speak with your future lives"
+  },
+  {
+    id: 75,
+    optionA: "Be able to see through walls",
+    optionB: "Be able to see through time"
+  },
+  {
+    id: 76,
+    optionA: "Have the power to make anyone rich",
+    optionB: "Have the power to make anyone famous"
+  },
+  {
+    id: 77,
+    optionA: "Be able to control the wind",
+    optionB: "Be able to control the rain"
+  },
+  {
+    id: 78,
+    optionA: "Have the ability to speak with your spirit guide",
+    optionB: "Have the ability to speak with your guardian angel"
+  },
+  {
+    id: 79,
+    optionA: "Be able to see through illusions",
+    optionB: "Be able to see through lies"
+  },
+  {
+    id: 80,
+    optionA: "Have the power to make anyone successful",
+    optionB: "Have the power to make anyone happy"
+  },
+  {
+    id: 81,
+    optionA: "Be able to control the earth",
+    optionB: "Be able to control the sky"
+  },
+  {
+    id: 82,
+    optionA: "Have the ability to speak with your past self",
+    optionB: "Have the ability to speak with your future self"
+  },
+  {
+    id: 83,
+    optionA: "Be able to see through time",
+    optionB: "Be able to see through space"
+  },
+  {
+    id: 84,
+    optionA: "Have the power to make anyone beautiful",
+    optionB: "Have the power to make anyone intelligent"
+  },
+  {
+    id: 85,
+    optionA: "Be able to control plants",
+    optionB: "Be able to control animals"
+  },
+  {
+    id: 86,
+    optionA: "Have the ability to speak with your younger self",
+    optionB: "Have the ability to speak with your older self"
+  },
+  {
+    id: 87,
+    optionA: "Be able to see through lies",
+    optionB: "Be able to see through illusions"
+  },
+  {
+    id: 88,
+    optionA: "Have the power to make anyone happy",
+    optionB: "Have the power to make anyone successful"
+  },
+  {
+    id: 89,
+    optionA: "Be able to control light",
+    optionB: "Be able to control darkness"
+  },
+  {
+    id: 90,
+    optionA: "Have the ability to speak with your past lives",
+    optionB: "Have the ability to speak with your future lives"
+  },
+  {
+    id: 91,
+    optionA: "Be able to see through walls",
+    optionB: "Be able to see through time"
+  },
+  {
+    id: 92,
+    optionA: "Have the power to make anyone rich",
+    optionB: "Have the power to make anyone famous"
+  },
+  {
+    id: 93,
+    optionA: "Be able to control the wind",
+    optionB: "Be able to control the rain"
+  },
+  {
+    id: 94,
+    optionA: "Have the ability to speak with your spirit guide",
+    optionB: "Have the ability to speak with your guardian angel"
+  },
+  {
+    id: 95,
+    optionA: "Be able to see through illusions",
+    optionB: "Be able to see through lies"
+  },
+  {
+    id: 96,
+    optionA: "Have the power to make anyone successful",
+    optionB: "Have the power to make anyone happy"
+  },
+  {
+    id: 97,
+    optionA: "Be able to control the earth",
+    optionB: "Be able to control the sky"
+  },
+  {
+    id: 98,
+    optionA: "Have the ability to speak with your past self",
+    optionB: "Have the ability to speak with your future self"
+  },
+  {
+    id: 99,
+    optionA: "Be able to see through time",
+    optionB: "Be able to see through space"
+  },
+  {
+    id: 100,
+    optionA: "Have the power to make anyone beautiful",
+    optionB: "Have the power to make anyone intelligent"
   }
 ]; 
